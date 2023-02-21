5d94aa1265b3d3185a8b2c03053488d0710cb96d
yglazer <121773896+yglazer@users.noreply.github.com>
2023-02-20 19:30:24 -0500

    trying to do carousel commented it out because cldnt do it

--- carousel.js
+++ carousel.js
@@ -1,3 +1,4 @@
+/*
 let slideIndex = 0;
 showSlides();
 
@@ -11,4 +12,4 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+}*/
